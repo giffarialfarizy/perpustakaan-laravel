5b6ce315648474814efa8dac4aafbdc4acee98c4
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-15 19:56:54 +0700

    ✨ [main] add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Perpustakaan</title>
+
+    @vite("resources/css/app.css")
+  </head>
+  <body class="antialiased">
+    <div>
+      <h1 class="text-2xl font-bold text-blue-600">Home</h1>
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('welcome');
-});
-
-// Route::get('/', [Controller::class, 'viewHome']);
+Route::get('/', [PublicController::class, 'viewHome']);
