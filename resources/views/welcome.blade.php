fe4474b8c934fc75dd0f4e32a1f7a54a38a157b2
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-15 17:29:19 +0700

    🎨 [main] format all resources files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Perpustakaan</title>
+    <title>Perpustakaan</title>
 
-        @vite('resources/css/app.css')
-    </head>
-    <body class="antialiased">
-        <div>
-            <h1 class="text-blue-600 font-bold text-2xl">Hello Laravel</h1>
-        </div>
-    </body>
+    @vite("resources/css/app.css")
+  </head>
+  <body class="antialiased">
+    <div>
+      <h1 class="text-2xl font-bold text-blue-600">Hello Laravel</h1>
+    </div>
+  </body>
 </html>
