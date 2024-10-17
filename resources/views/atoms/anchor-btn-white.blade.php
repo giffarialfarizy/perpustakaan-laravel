65de4e2a2c636eb7469f9c03f651b5d597943179
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-18 06:36:05 +0700

    ✨ [components][atoms] add anchor-btn
    - anchor-btn-green
    - anchor-btn-white

---
+++ resources/views/atoms/anchor-btn-white.blade.php
@@ -0,0 +1,6 @@
+<a
+  href="{{ $href }}"
+  class="mb-2 me-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100"
+>
+  {{ $text }}
+</a>
