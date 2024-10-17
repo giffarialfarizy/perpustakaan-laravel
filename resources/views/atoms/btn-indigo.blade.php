fd4b3c2cf8472a63e465c6defbaeb9931fbed060
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-18 06:36:53 +0700

    ✨ [components][atoms] add btn-indigo

---
+++ resources/views/atoms/btn-indigo.blade.php
@@ -0,0 +1,6 @@
+<button
+  type="{{ $type }}"
+  class="rounded-lg bg-indigo-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 sm:w-auto"
+>
+  {{ $text }}
+</button>
