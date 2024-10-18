edd6516ee892df723fb829d8b0f83d092466fb7f
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-19 06:47:44 +0700

    ✨ [components][atoms] change anchor-btn-green color to emerald

--- resources/views/atoms/anchor-btn-green.blade.php
+++ resources/views/atoms/anchor-btn-green.blade.php
@@ -1,6 +1,6 @@
 <a
   href="{{ $href }}"
-  class="mb-2 me-2 mt-4 rounded-lg bg-green-500 p-4 px-5 py-2.5 text-sm font-medium text-white hover:bg-green-400 focus:outline-none focus:ring-4 focus:ring-green-300"
+  class="mb-2 me-2 mt-4 rounded-lg bg-emerald-500 p-4 px-5 py-2.5 text-sm font-medium text-white hover:bg-emerald-400 focus:outline-none focus:ring-4 focus:ring-emerald-300"
 >
   {{ $text }}
 </a>
