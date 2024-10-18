9394f2460b1ef237f4bc4bcd530c64159212b6b7
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-18 07:46:29 +0700

    ✨ [components][atoms] add icons
    icon list:
    - book
    - borrowing
    - category
    - dashboard
    - setting
    - sidebar-toggle
    - sort

---
+++ resources/views/atoms/icons/dashboard.blade.php
@@ -0,0 +1,18 @@
+<svg
+  class="{{ $class }}"
+  aria-hidden="true"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="currentColor"
+  viewBox="0 0 24 24"
+>
+  <g
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+  >
+    <path d="M11 3.055A9.001 9.001 0 1 0 20.945 13H11z" />
+    <path d="M20.488 9H15V3.512A9.03 9.03 0 0 1 20.488 9" />
+  </g>
+</svg>
