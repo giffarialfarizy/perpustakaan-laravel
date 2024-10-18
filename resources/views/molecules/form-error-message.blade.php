196049cf1f094a1157615f4ca38db3670b88e93e
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-19 06:48:35 +0700

    ✨ [components][molecules] change form-error-message background color to rose

--- resources/views/molecules/form-error-message.blade.php
+++ resources/views/molecules/form-error-message.blade.php
@@ -1,4 +1,4 @@
-<div class="mb-4 rounded bg-red-500 p-2 text-sm">
+<div class="mb-4 rounded bg-rose-500 p-2 text-sm">
   <ul>
     @foreach ($errors->all() as $error)
       <li class="text-white">{{ $error }}</li>
