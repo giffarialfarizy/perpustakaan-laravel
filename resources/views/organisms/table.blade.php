c1ad1f3017ada4a24b1a8f0808898128a75f27b7
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-10-19 07:53:48 +0700

    ✨ [components][organisms] add table

---
+++ resources/views/organisms/table.blade.php
@@ -0,0 +1,52 @@
+<table id="{{ $id }}">
+  <thead>
+    <tr>
+      <th scope="col"><span class="flex items-center">#</span></th>
+
+      @foreach ($headers as $header)
+        <th>
+          <span class="flex items-center">
+            {{ $header["label"] }}
+            @if ($header["sortable"])
+              @include("atoms.icons.sort", ["class" => "ms-1 h-4 w-4"])
+            @endif
+          </span>
+        </th>
+      @endforeach
+
+      @if ($action["show"] === true)
+        @include("molecules.table.action-header")
+      @endif
+    </tr>
+  </thead>
+
+  <tbody>
+    @forelse ($datas as $data)
+      <tr>
+        <th scope="row">{{ $loop->iteration }}</th>
+
+        @foreach ($headers as $header)
+          <td class="whitespace-nowrap font-medium text-gray-900">
+            {{ $data->{$header["field"]} ?? "-" }}
+          </td>
+        @endforeach
+
+        @if ($action["show"] === true)
+          @include(
+            "molecules.table.action-body",
+            [
+              "hrefView" => $action["hrefView"],
+              "hrefEdit" => $action["hrefEdit"],
+              "hrefRemove" => $action["hrefRemove"],
+              "actionRemove" => $action["actionRemove"],
+            ]
+          )
+        @endif
+      </tr>
+    @empty
+      <tr>
+        <td class="text-gray-300">Tidak ada data</td>
+      </tr>
+    @endforelse
+  </tbody>
+</table>
